92efc34996f97efa006f85cbcce2531f3ce0bdf5
Tseku <otgonbaatartsenguun@yahoo.com>
2024-07-06 17:26:45 +0800

    coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='relative flex items-center justify-center h-screen w-screen bg-white'>
+      <div className='absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]'></div>
+      <div className='flex h-screen w-full items-center justify-center bg-transparent'>
+        <span className='absolute mx-auto py-4 flex border w-fit bg-gradient-to-r blur-3xl from-amber-500 to-pink-500 bg-clip-text text-9xl box-content font-extrabold text-transparent text-center select-none'>
+          Coming soon...
+        </span>
+        <h1 className='relative top-0 w-fit h-auto py-4 justify-center flex bg-gradient-to-r items-center from-amber-500 to-pink-500 bg-clip-text text-9xl font-extrabold text-transparent text-center select-auto'>
+          Coming soon...
+        </h1>
+      </div>
     </div>
   );
 }
